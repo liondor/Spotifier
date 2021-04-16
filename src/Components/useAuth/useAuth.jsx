e5abba72af19bf5c8010e52ba0f62796a1390648
liondor <johen.sylvestre@gmail.com>
2021-04-16 14:19:55 +0200

    Ajout d'un useEffect pour refresh notre acessToken dans useAuth

--- src/Components/useAuth/useAuth.jsx
+++ src/Components/useAuth/useAuth.jsx
@@ -12,7 +12,33 @@ export default function useAuth({code}) {
                 code:code,
         }).then(res=>{
             console.log(res.data)
+            setAccessToken(res.data.accessToken)
+            setRefreshToken(res.data.refreshToken)
+            setExpiresIn(res.data.expiresIn)
+            window.history.pushState({},null,"/")
+        }).catch(() => {
+            window.location="/"
         })
         
     }, [code])
+
+    
+    useEffect(() => {
+        console.log({code})
+        axios
+            .post('http://localhost:3002/refresh', {
+                refreshToken,
+        }).then(res=>{
+            console.log(res.data)
+     /*       setAccessToken(res.data.accessToken)
+            setRefreshToken(res.data.refreshToken)
+            setExpiresIn(res.data.expiresIn)
+            window.history.pushState({},null,"/")
+            */
+        }).catch(() => {
+            window.location="/"
+        })
+        
+    }, [refreshToken, expiresIn])
+    return accessToken
 }
